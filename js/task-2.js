ad346b47a1114a5a895f9313bdf565c999bdd70b
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-07-17 22:34:33 +0300

    add first commit

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function receiveUser(user) {
+  return user !== null ? user.username : "no user found";
+}
+
+console.log(receiveUser(null));
+
+// a || b || c || d    повертає пертого thuthy або останній
+// a && b && c && d    повертає першого falthy або останній
+// a ?? b ?? c ?? d    повертає першого не-null або не-undefined або останній
